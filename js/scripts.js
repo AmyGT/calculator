d15eb89da91eb99d4d70e8581654a9487ce46e22
AmyGT <amygabriellathomas@gmail.com>
2023-10-02 22:16:02 -0700

    Add alerts to return results of functions to user.

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,9 @@ const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1, number2));
+
+window.alert(subtract(number1, number2));
+
+window.alert(multiply(number1, number2));
+
+window.alert(divide(number1, number2));
