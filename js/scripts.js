2d599c56ddc590fc918abb59845f3f0f073df081
AmyGT <amygabriellathomas@gmail.com>
2023-10-03 17:44:41 -0700

    Combine results return into one alert.

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,4 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
-
-window.alert(subtract(number1, number2));
-
-window.alert(multiply(number1, number2));
-
-window.alert(divide(number1, number2));
+window.alert("The sum of your numbers equals " + add(number1, number2) + ". " + "The subtraction of your numbers equals " + subtract(number1, number2) + ". " + "The product of your numbers equals " + multiply(number1, number2) + ". " + "The division of your numbers equals " + divide(number1, number2) + ".");
